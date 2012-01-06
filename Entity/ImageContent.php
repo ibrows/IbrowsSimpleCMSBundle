5c5dd4ab9ca97337dde2fb19d4f96806cb00ea8d
Ibrows <info@ibrows.ch>
2012-01-06 11:23:41 +0100

    add perm-links

--- Entity/ImageContent.php
+++ Entity/ImageContent.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Ibrows\SimpleCMSBundle\Entity;
-
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -86,8 +85,11 @@ class ImageContent extends Content
         $this->path = ''; //path must be changed when file change
     }
 
-    public function getWebPath()
+    public function getWebPath($permlink = false)
     {
+        if ($permlink) {
+            return $this->getSymLink(true);
+        }
         return null === $this->getPath() ? null : $this->getPath();
     }
 
@@ -101,13 +103,14 @@ class ImageContent extends Content
         // the absolute directory path where uploaded documents should be saved
         //rootpath
 
-        return $this->params->getParameter('kernel.root_dir') . '/../web/' . $this->getPath();
+        return $this->params
+                ->getParameter('kernel.root_dir') . '/../web/' . $this->getPath();
     }
 
     protected function getUploadDir()
     {
-        // get rid of the __DIR__ so it doesn't screw when displaying uploaded doc/image in the view.
-        return $this->params->getParameter('ibrows_simple_cms.upload_dir');
+        return $this->params
+                ->getParameter('ibrows_simple_cms.upload_dir');
     }
 
     /**
@@ -119,11 +122,14 @@ class ImageContent extends Content
 
         if (null !== $this->file) {
             $name = preg_replace('/([^a-z0-9\-\_])/i', '', $this->getName());
-            $xtension = $this->file->guessExtension();
+            $xtension = $this->file
+                    ->guessExtension();
             if ($xtension == null) {
-                $xtension = $this->file->getExtension();
+                $xtension = $this->file
+                        ->getExtension();
                 if ($xtension == null) {
-                    $xtension = ExtensionGuesser::getInstance()->guess($this->file->getClientMimeType());
+                    $xtension = ExtensionGuesser::getInstance()->guess($this->file
+                                    ->getClientMimeType());
                 }
             }
             $this->setPath($this->getUploadDir() . '/' . $name . time() . '.' . $xtension);
@@ -141,9 +147,28 @@ class ImageContent extends Content
             return;
         }
         $filename = str_replace(dirname($this->getAbsolutePath()), '', $this->getAbsolutePath());
-        $this->file->move($this->getUploadRootDir(), $filename);
+        $this->file
+                ->move($this->getUploadRootDir(), $filename);
 
+        if (is_file($this->getSymLink())) {
+            unlink($this->getSymLink());
+        }
+        if (!is_dir(dirname($this->getSymLink()))) {
+            mkdir(dirname($this->getSymLink()));
+        }
+        link($this->getAbsolutePath(), $this->getSymLink());
         unset($this->file);
+    }
+
+    public function getSymLink($relative = false)
+    {
+        $name = preg_replace('/([^a-z0-9\-\_])/i', '', $this->getName());
+        $ext = strrchr($this->getPath(), '.');
+        if (!$relative) {
+            return dirname($this->getAbsolutePath()) . '/sym/' . $name . $ext;
+        } else {
+            return dirname($this->getPath()) . '/sym/' . $name . $ext;
+        }
     }
 
     /**
@@ -151,9 +176,13 @@ class ImageContent extends Content
      */
     public function removeUpload()
     {
-        if ($file = $this->getAbsolutePath()) {
-            unlink($file);
+        if (is_file($this->getAbsolutePath())) {
+            unlink($this->getAbsolutePath());
         }
+        if (is_file($this->getSymLink())) {
+            unlink($this->getSymLink());
+        }
+
     }
 
     //return html
@@ -162,33 +191,50 @@ class ImageContent extends Content
         $return = '';
         $name = $filter->filterHtml($this->getName());
         $description = $filter->filterHtml($this->getDescription());
-        if(empty($description)){
+        if (empty($description)) {
             $description = $name;
         }
-        $config = array('attr' => array('class' => 'simplecms-imagecontent', 'alt' => $description, 'title' => $name));
-        $image = true;
-        try {
-            $mimetpye = \Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesser::getInstance()->guess($this->getWebPath());
-            if (strpos($mimetpye, 'image') !== 0) {
-                $image = false;
+        $config = array(
+            'attr' => array(
+                'class' => 'simplecms-imagecontent', 'alt' => $description, 'title' => $name
+            )
+        );
+
+        if (isset($args['forceimage']) && $args['forceimage'] == true) {
+            $image = true;
+        } else if (isset($args['forcefile']) && $args['forcefile'] == true) {
+            $image = false;
+        } else {
+            try {
+                $mimetpye = \Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesser::getInstance()->guess($this->getWebPath());
+                if (strpos($mimetpye, 'image') !== 0) {
+                    $image = false;
+                } else {
+                    $image = true;
+                }
+            } catch (\Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException $e) {
+                $image = true;
             }
-        } catch (\Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException $e) {
-            
         }
         if (!$image) {
             //if not a image
-            $config = array('attr' => array('class' => 'simplecms-downloadcontent', 'onclick' => 'window.open(this.href); return', 'title' => $name));
+            $config = array(
+                'attr' => array(
+                    'class' => 'simplecms-downloadcontent', 'onclick' => 'window.open(this.href); return', 'title' => $name
+                )
+            );
         }
         $arr = parent::mergeUserArgs($args, $config);
         foreach ($arr['attr'] as $key => $val) {
             $return .= " $key=\"$val\"";
         }
+        $path = $this->getWebPath((isset($args['permlink']) && $args['permlink'] == true));
 
         if (!$image) {
             //if not a image
-            return '<a href="/' . $this->getWebPath() . '" ' . $return . ' >' . $name . ' </a>';
+            return '<a href="/' . $path . '" ' . $return . ' >' . $name . ' </a>';
         }
-        $return = '<img src="/' . $this->getWebPath() . '" ' . $return . ' >';
+        $return = '<img src="/' . $path . '" ' . $return . ' >';
         return $return;
         ;
     }
