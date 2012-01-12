8bb6ea4a0c29da996a2f3ad83870551050d948b9
Ibrows <info@ibrows.ch>
2012-01-12 11:32:22 +0100

    use target

--- Entity/ImageContent.php
+++ Entity/ImageContent.php
@@ -220,7 +220,7 @@ class ImageContent extends Content
             //if not a image
             $config = array(
                     'attr' => array(
-                        'class' => 'simplecms-downloadcontent', 'onclick' => 'window.open(this.href); return', 'title' => $name
+                        'class' => 'simplecms-downloadcontent', 'target' => '_blank!', 'title' => $name
                     )
             );
         }
