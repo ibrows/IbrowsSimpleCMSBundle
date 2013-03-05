4f4ef60dec060813670c4f822e4a01053e6eb1cd
Mike Meier <mike.meier@ibrows.ch>
2013-03-05 17:18:32 +0100

    bugfix new doGenerate signature

--- Routing/UrlGenerator.php
+++ Routing/UrlGenerator.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 namespace Ibrows\SimpleCMSBundle\Routing;
 
 use Symfony\Component\Routing\Route;
@@ -12,36 +10,35 @@ use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
 
 /**
- *
  * @api
  */
 class UrlGenerator extends \Symfony\Component\Routing\Generator\UrlGenerator
 {
     const GENERATE_NORMAL_ROUTE = '!!!';
 
-    protected function doGenerate($variables, $defaults, $requirements, $tokens, $parameters, $name, $absolute)
+    protected function doGenerate($variables, $defaults, $requirements, $tokens, $parameters, $name, $referenceType, $hostTokens)
     {
-        if(key_exists(self::GENERATE_NORMAL_ROUTE, $parameters) ){
+        if(array_key_exists(self::GENERATE_NORMAL_ROUTE, $parameters) ){
             unset($parameters[self::GENERATE_NORMAL_ROUTE]);
-            return parent::doGenerate($variables, $defaults, $requirements, $tokens, $parameters, $name, $absolute);
+            return parent::doGenerate($variables, $defaults, $requirements, $tokens, $parameters, $name, $referenceType, $hostTokens);
         }
-        
+
         //use the cached version with my name - do the standard request if its not work
         // dont generate assets                
         if (stripos($name, RouteLoader::ROUTE_BEGIN) !== 0 && stripos($name, '_assetic') !== 0) {
             try {
-                $route = RouteLoader::getRouteName($name, array_merge($this->context->getParameters(), $parameters,$defaults ));
-                return $this->generate( $route, $parameters, $absolute);
+                $route = RouteLoader::getRouteName($name, array_merge($this->context->getParameters(), $parameters, $defaults ));
+                return $this->generate( $route, $parameters, $referenceType );
             } catch (RouteNotFoundException $e) {
                 try {
                     $reqparams = array();
                     foreach ($requirements as $key => $val){
-                        if(key_exists($key, $parameters)){
+                        if(array_key_exists($key, $parameters)){
                             $reqparams[$key] = $parameters[$key];
                         }
                     }
-                    $route = RouteLoader::getRouteName($name, array_merge($this->context->getParameters(), $reqparams,$defaults ));
-                    return $this->generate( $route, $parameters, $absolute);
+                    $route = RouteLoader::getRouteName($name, array_merge($this->context->getParameters(), $reqparams, $defaults ));
+                    return $this->generate( $route, $parameters, $referenceType );
                 } catch (RouteNotFoundException $e) {
                     // do nothing, go on and do the normal Request
                 }
@@ -49,7 +46,7 @@ class UrlGenerator extends \Symfony\Component\Routing\Generator\UrlGenerator
         }
 
 
-        return parent::doGenerate($variables, $defaults, $requirements, $tokens, $parameters, $name, $absolute);
+        return parent::doGenerate($variables, $defaults, $requirements, $tokens, $parameters, $name, $referenceType, $hostTokens);
     }
 
 }
