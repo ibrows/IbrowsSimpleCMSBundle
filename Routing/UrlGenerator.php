68551d1f53ae9a21e15a854f6a127b1de31fcd7b
Gladhon <marc.steiner@ibrows.ch>
2013-06-27 19:50:32 +0200

    fix alias generation

--- Routing/UrlGenerator.php
+++ Routing/UrlGenerator.php
@@ -1,9 +1,6 @@
 <?php
 
-
-
 namespace Ibrows\SimpleCMSBundle\Routing;
-
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\RequestContext;
@@ -21,33 +18,43 @@ class UrlGenerator extends \Symfony\Component\Routing\Generator\UrlGenerator
 
     protected function doGenerate($variables, $defaults, $requirements, $tokens, $parameters, $name, $absolute)
     {
-        if(key_exists(self::GENERATE_NORMAL_ROUTE, $parameters) ){
+        if (key_exists(self::GENERATE_NORMAL_ROUTE, $parameters)) {
             unset($parameters[self::GENERATE_NORMAL_ROUTE]);
             return parent::doGenerate($variables, $defaults, $requirements, $tokens, $parameters, $name, $absolute);
         }
-        
+
         //use the cached version with my name - do the standard request if its not work
-        // dont generate assets                
+        // dont generate assets
         if (stripos($name, RouteLoader::ROUTE_BEGIN) !== 0 && stripos($name, '_assetic') !== 0) {
             try {
-                $route = RouteLoader::getRouteName($name, array_merge($this->context->getParameters(), $parameters,$defaults ));
-                return $this->generate( $route, $parameters, $absolute);
+                $route = RouteLoader::getRouteName($name, array_merge($this->context->getParameters(), $parameters, $defaults));
+                return $this->generate($route, $parameters, $absolute);
             } catch (RouteNotFoundException $e) {
                 try {
                     $reqparams = array();
-                    foreach ($requirements as $key => $val){
-                        if(key_exists($key, $parameters)){
-                            $reqparams[$key] = $parameters[$key];
+                    foreach ($variables as $val) {
+                        if (key_exists($val, $parameters)) {
+                            $reqparams[$val] = $parameters[$val];
                         }
                     }
-                    $route = RouteLoader::getRouteName($name, array_merge($this->context->getParameters(), $reqparams,$defaults ));
-                    return $this->generate( $route, $parameters, $absolute);
+                    $route = RouteLoader::getRouteName($name, array_merge($this->context->getParameters(), $reqparams, $defaults));
+                    return $this->generate($route, $parameters, $absolute);
                 } catch (RouteNotFoundException $e) {
-                    // do nothing, go on and do the normal Request
+                    try {
+                        $reqparams = array();
+                        foreach ($requirements as $key => $val) {
+                            if (key_exists($key, $parameters)) {
+                                $reqparams[$key] = $parameters[$key];
+                            }
+                        }
+                        $route = RouteLoader::getRouteName($name, array_merge($this->context->getParameters(), $reqparams, $defaults));
+                        return $this->generate($route, $parameters, $absolute);
+                    } catch (RouteNotFoundException $e) {
+                        // do nothing, go on and do the normal Request
+                    }
                 }
             }
         }
-
 
         return parent::doGenerate($variables, $defaults, $requirements, $tokens, $parameters, $name, $absolute);
     }
